26257cd461f124d9508356ed99f1ccafc9bd56c5
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-08 12:22:00 -0300

    Agregue funcionalidad de noticias dinámicas: Utilice useState y useEffect para gestionar el estado de 'categoria' y 'noticias', llame a la API de noticia, realice un array 'CATEGORIAS' con opciones de categorías

--- src/components/FormularioNoticias.jsx
+++ src/components/FormularioNoticias.jsx
@@ -1,6 +1,32 @@
+import { useState,useEffect } from "react";
 import { Container, Form } from "react-bootstrap";
 
 const FormularioNoticias = () => {
+  const [categoria, setCategoria] = useState();
+  const [noticias, setNoticias] = useState([]);
+
+  const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=de7790c8316344f1aad516564e45553f`;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const respuesta = await fetch(url);
+      const data = await respuesta.json();
+
+      setNoticias(data.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
+
+  const Categorias = [
+    { value: "portada", label: "Portada" },
+    { value: "ultimo momento", label: "Ultimo momento" },
+    { value: "deportes", label: "Deportes" },
+    { value: "politica", label: "Politica" },
+    { value: "seguridad", label: "Seguridad" },
+    { value: "economía", label: "Economía" },
+    { value: "cultura", label: "Cultura" },
+  ];
+
   return (
     <>
       <Container className="border border-1 mt-5">
@@ -8,11 +34,15 @@ const FormularioNoticias = () => {
           <Form.Label className="w-50 ms-2 mb-2 mb-md-0 ">
             Busca por Categoría:
           </Form.Label>
-          <Form.Select>
-            <option>Opciones</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+          <Form.Select
+            value={categoria}
+            onChange={(e) => setCategoria(e.target.value)}
+          >
+            {Categorias.map((categoria) => (
+              <option key={categoria.value} value={categoria.value}>
+                {categoria.label}
+              </option>
+            ))}
           </Form.Select>
         </Form.Group>
       </Container>
